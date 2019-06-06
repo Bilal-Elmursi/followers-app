02d9e2e8c8a088c88520b0b8d5da6303fa2cff5e
Bilal El-Mursi <bilalelmursi@gmail.com>
2019-06-06 16:45:45 +0200

    Auto-generated commit

--- out-tsc/src/app/format-text.directive.spec.js
+++
@@ -1,8 +0,0 @@
-import { FormatTextDirective } from './format-text.directive';
-describe('FormatTextDirective', function () {
-    it('should create an instance', function () {
-        var directive = new FormatTextDirective();
-        expect(directive).toBeTruthy();
-    });
-});
-//# sourceMappingURL=format-text.directive.spec.js.map
